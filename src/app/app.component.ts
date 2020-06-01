78798cb83e17d3e194436464d0d826f6bfd086e6
JGhignatti <joao.ghignatti@inf.ufrgs.br>
2020-06-01 00:14:24 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'jv-weather';
+}
